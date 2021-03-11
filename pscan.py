88b897e981e73e45c2ea67d25506a37532a5f9d8
Christopher Steffen <cmsteffen@haxys.net>
2021-03-10 22:10:54 -0600

    Implemented ping and hostname checks.

--- pscan.py
+++ pscan.py
@@ -3,9 +3,13 @@
 """Pscan: A fast Python port scanner."""
 
 import argparse
+import datetime
 import os
+import socket
 import sys
 import textwrap
+
+from scapy.all import conf, sr1, ICMP, IP
 
 
 PORT_RANGE_FORMAT = textwrap.dedent(
@@ -34,6 +38,15 @@ def parse_ports(port_string):
     return sorted(ports)
 
 
+def ping(hostname):
+    """Ping the target host. Return either the ping delay or None."""
+    conf.verb = 0
+    start = datetime.datetime.now()
+    online = sr1(IP(dst=hostname) / ICMP(), timeout=3) is not None
+    end = datetime.datetime.now()
+    return int((end - start).total_seconds() * 1000) if online else None
+
+
 def scan(hostname, port_range):
     """Scan the specified ports on the target host."""
     _ = (hostname, port_range)
@@ -51,18 +64,52 @@ def main():
         epilog=PORT_RANGE_FORMAT,
     )
     parser.add_argument(
+        "-N", "--no-ping", default=True, action="store_const", const=False, help="Skip initial ping.", dest="ping",
+    )
+    parser.add_argument(
         "hostname", type=str, help="The hostname or IP to scan."
     )
     parser.add_argument(
         "port_string", type=str, help="The port range to scan."
     )
     args = parser.parse_args()
+
     if os.getuid() != 0:
         print("[!] This script requires root privileges.")
         sys.exit(1)
+
+    try:
+        socket.gethostbyname(args.hostname)
+    except socket.gaierror:
+        print(f"[!] Could not resolve hostname: {args.hostname}")
+        sys.exit(1)
+
+    if args.ping:
+        latency = ping(args.hostname)
+        if not latency:
+            print("[!] No ping response from target host.")
+            sys.exit(1)
+        print(f"[*] Ping: {latency}ms")
+    else:
+        print("[*] Skipping initial ping.")
+
     ports = parse_ports(args.port_string)
     print(f"[*] Scanning {len(ports)} ports on {args.hostname}")
+    start = datetime.datetime.now()
     results = scan(args.hostname, ports)
+    end = datetime.datetime.now()
+    delta = end-start
+    hours = delta.seconds // 3600
+    minutes = (delta.seconds // 60) - (hours * 60)
+    seconds = delta.seconds - (minutes * 60) - (hours * 3600)
+    scan_time = f"[*] Scan completed in " + (
+        f"{hours} hours, " if hours else ""
+    ) + (
+        f"{minutes} minutes, " if minutes else ""
+    ) + (
+        f"{seconds}.{str(delta.microseconds)[:3]} seconds"
+    )
+    print(scan_time)
     print("[*] Results:")
     for port, result in results.items():
         print(f"{' ' * 4}{port}{' ' * (6-len(str(port)))}{result}")
